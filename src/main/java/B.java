3ad72cf578a91b149d468492e272934b129b4858
yuhuigit <yuhuishijava@163.com>
2016-09-06 12:52:50 +0800

    first

--- src/main/java/B.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2016/9/6.
- */
-public class B {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/C.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2016/9/6.
- */
-public class C {
-    public static void main(String[] args) {
-        System.out.printf("c");
-    }
-}
