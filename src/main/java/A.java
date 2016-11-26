3cd60a8151f934f1f934d2b63abf2653aaeb77a2
yuhuigit <yuhuishijava@163.com>
2016-11-26 21:08:58 +0800

    Revert "11"
    This reverts commit b789272b14f5d1505a74bb10c9c44956c968715b.
    
    # Conflicts:
    #	src/main/java/A.java

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -4,7 +4,6 @@
 public class A {
     public static void main(String[] args) {
         System.out.printf("nihao");
-        System.out.println("1111");
-        System.out.println("ss");
+        System.out.println("123");
     }
 }
